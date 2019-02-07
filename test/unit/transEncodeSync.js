fe00968175ec05c9132b01328af810ce3e7baa78
Tim Popham <popham@uw.edu>
2019-02-07 15:54:48 -0800

    Switch to Mocha and Istanbul

--- test/unit/transEncodeSync.js
+++ test/unit/transEncodeSync.js
@@ -1,42 +1,45 @@
 /* @flow */
 
-import test from "ava";
+import * as assert from "assert";
 import Prando from "prando";
+import { describe, it } from "mocha";
 import { array } from "@capnp-js/trans-inject";
 
-import transEncodeSync from "../../lib/transEncodeSync";
+import transEncodeSync from "../../src/transEncodeSync";
 
-test("`transEncodeSync`", t => {
+describe("transEncodeSync", function () {
   const random = new Prando(613);
 
   for (let i=0; i<5000; ++i) {
-    const length = random.nextInt(0, 200);
-    const raw = "x".repeat(length);
+    it(`decodes random data i=${i}`, function () {
+      const length = random.nextInt(0, 200);
+      const raw = "x".repeat(length);
 
-    const buffers = [];
-    let cut = 0;
-    while (cut < length) {
-      const next = Math.min(cut + random.nextInt(0, 50), length);
-      buffers.push(raw.slice(cut, next));
-      cut = next;
-    }
-
-    const mod = random.nextInt(1, 15);
-    const align = transEncodeSync(mod);
-    const encoding = align(array(buffers));
-    let i = encoding.next();
-    let failed = false;
-    while (!i.done) {
-      if (i.value.length % mod === 0) {
-        i = encoding.next();
-        t.true(true);
-      } else {
-        /* Only the final iteration can provide an unaligned buffer. */
-        i = encoding.next();
-        t.true(i.done);
+      const buffers = [];
+      let cut = 0;
+      while (cut < length) {
+        const next = Math.min(cut + random.nextInt(0, 50), length);
+        buffers.push(raw.slice(cut, next));
+        cut = next;
       }
-    }
 
-    t.false(i.done instanceof Error);
+      const mod = random.nextInt(1, 15);
+      const align = transEncodeSync(mod);
+      const encoding = align(array(buffers));
+      let i = encoding.next();
+      let failed = false;
+      while (!i.done) {
+        if (i.value.length % mod === 0) {
+          i = encoding.next();
+          assert.ok(true);
+        } else {
+          /* Only the final iteration can provide an unaligned buffer. */
+          i = encoding.next();
+          assert.ok(i.done);
+        }
+      }
+
+      assert.ok(!(i.done instanceof Error));
+    });
   }
 });
